904b287b6729d3c61469f3c439659e55387e4e16
jonasLac <jonasfamilia262@gmail.com>
2026-02-12 15:41:06 -0300

    Minhas mudanças locais antes do lovable

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.d5b2c014c9ed4f119682aa8bef7ac5fc',
-  appName: 'focusflow-buddy-03',
+  appId: 'com.fokko.app',
+  appName: 'Fokko',
   webDir: 'dist',
-  server: {
-    url: 'https://d5b2c014-c9ed-4f11-9682-aa8bef7ac5fc.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
 };
 
 export default config;
